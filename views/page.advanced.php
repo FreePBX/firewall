d35be94568f1a1d370370f8ee5570286db36b673
Rob Thomas <xrobau@gmail.com>
2015-08-13 13:01:42 +1000

    Remove 'networks', make it part of Zones

--- views/page.advanced.php
+++ views/page.advanced.php
@@ -1,10 +1,9 @@
-<script type='text/javascript' src='modules/firewall/assets/js/views/networks.js'></script>
 <form method='post'>
 <div class="display no-border">
   <div class="nav-container">
     <ul class="nav nav-tabs list" role="tablist">
-      <li role="presentation" data-name="networks" class="active">
-        <a href="#networks" aria-controls="networks" role="tab" data-toggle="tab"><?php echo _("Networks")?> </a>
+      <li role="presentation" data-name="advanced" class="active">
+        <a href="#advanced" aria-controls="advanced" role="tab" data-toggle="tab"><?php echo _("Advanced")?> </a>
       </li>
       <li role="presentation" data-name="shortcuts">
         <a href="#shortcuts" aria-controls="shortcuts" role="tab" data-toggle="tab"><?php echo _("Preconfigured")?> </a>
@@ -15,68 +14,9 @@
     </ul>
     <div class="tab-content display">
       <div role="tabpanel" id="networks" class="tab-pane active">
-	<p><?php echo _("Individual networks may be specified to override the default rule for an interface. For example, if interface eth0 is assigned to the 'Reject' zone (meaning everything is rejected by default), you could add a specific source network to the 'Trusted' zone. This will override the default rule for that interface, so traffic from that network will be treated as 'Trusted',  whilst all traffic NOT originating from that network is Rejected."); ?></p>
-        <p><?php echo _("Note that several common settings are available in the 'Preconfigured' tab."); ?></p>
+        <p>Text for advanved goes here...</p>
+      </div>
 <?php
-$nets = $fw->getZoneNetworks();
-// Add a blank one to the bottom..
-$nets[" "] = "trusted";
-
-$z = $fw->getZones();
-
-// Now, loop through our networks and display them.
-$counter = 1;
-foreach ($nets as $net => $currentzone) {
-	if (trim($net)) {
-		$ro = "readonly disabled";
-	} else {
-		$ro = "";
-	}
-?>
-<div class='element-container' id='element-<?php echo $counter; ?>'>
-  <div class='row'>
-    <div class='col-sm-4 col-md-3'>
-      <input type='text' name='netname[<?php echo $counter; ?>]' value='<?php echo $net; ?>' <?php echo $ro; ?>>
-    </div>
-    <div class='col-sm-8 col-md-9'>
-      <span class='radioset'>
-<?php
-	// Display the buttons
-	foreach ($z as $zn => $zone) {
-		if ($zn == "reject") {
-			// Don't show 'reject', as it's not intuitive. It adds a network to an
-			// interface that's defined as reject, so that'll be PERMITTED, and..
-			// yeah. Let's just avoid that.
-			continue;
-		}
-		if ($zn === $currentzone) {
-			$active = "active";
-			$checked = "checked";
-		} else {
-			$active = "";
-			$checked = "";
-		}
-		print "<input type='radio' name='net-$counter' id='net-$counter-$zn' value='$zn' $checked><label class='$active' for='net-$counter-$zn'>".$zone['name']."</label>\n";
-	}
-?>
-      </span>
-<?php
-	// Add the 'remove' X if the net isn't empty
-	if (trim($net)) {
-		print "<button type='button' class='btn x-btn x-btn-danger fwbutton' data-counter='$counter' data-action='remove'><span class='glyphicon glyphicon-remove'></span></button>";
-		print "<button type='button' class='btn x-btn x-btn-warning fwbutton' data-counter='$counter' data-action='update'><span class='glyphicon glyphicon-pencil'></span></button>";
-	} else {
-		// Or a '+' add if it is.
-		print "<button type='button' class='btn btn-success x-btn x-btn-success fwbutton' data-counter='$counter' data-action='create'><span class='glyphicon glyphicon-plus'></span></button>";
-	}
-?>
-    </div>
-  </div>
-</div>
-<?php
-$counter++;
-} // foreach nets
-
 // Text for Shortcuts is here
 $rfc = array(
 	_("RFC1918"),
@@ -95,7 +35,6 @@ $thisnet = $fw->detectNetwork();
 $thishost = $fw->detectHost();
 
 ?>
-      </div>
       <div role="tabpanel" id="shortcuts" class="tab-pane">
         <div class='container-fluid'>
 	  <p><?php echo _("This allows you to simply add a pre-configured set of networks to your trusted zone. Once you have added your selections, you can fine-tune them, if required, on the Networks tab."); ?></p>
--- views/page.zones.php
+++ views/page.zones.php
@@ -1,3 +1,4 @@
+<script type='text/javascript' src='modules/firewall/assets/js/views/networks.js'></script>
 <form method='post'>
 <div class="display no-border">
   <div class="nav-container">
@@ -5,8 +6,11 @@
       <li role="presentation" data-name="zonedocs" class="active">
         <a href="#zonedocs" aria-controls="zonedocs" role="tab" data-toggle="tab"><?php echo _("Zone Information")?> </a>
       </li>
-      <li role="presentation" data-name="zonesettings">
-        <a href="#zonesettings" aria-controls="zonesettings" role="tab" data-toggle="tab"><?php echo _("Zone Assignments")?> </a>
+      <li role="presentation" data-name="intsettings">
+        <a href="#intsettings" aria-controls="intsettings" role="tab" data-toggle="tab"><?php echo _("Interfaces")?> </a>
+      </li>
+      <li role="presentation" data-name="netsettings">
+        <a href="#netsettings" aria-controls="netsettings" role="tab" data-toggle="tab"><?php echo _("Networks")?> </a>
       </li>
     </ul>
     <div class="tab-content display">
@@ -23,8 +27,8 @@ echo "</ul>";
 ?>
 
       </div>
-      <div role="tabpanel" id="zonesettings" class="tab-pane">
-      <p><?php echo _("Please assign a zone to all interfaces. Note that 'Trusted' means that <strong>no filtering</strong> will be applied to this interface. 'Reject' means <strong>no inbound connections will be permitted</strong> to that interface."); ?></p>
+      <div role="tabpanel" id="intsettings" class="tab-pane">
+        <p><?php echo _("All interfaces must be assigned to a default zone. Any traffic entering this interface (that does not originate from a network in the Networks tab) is firewalled to the rules of that zone. Note that 'Trusted' means that <strong>no filtering</strong> will be applied to this interface. 'Reject' means <strong>no inbound connections will be permitted</strong> to that interface."); ?></p>
 <?php
 $ints = $fw->getInterfaces();
 
@@ -73,6 +77,71 @@ foreach ($ints as $i => $conf) {
 </div>
 <?php // foreach ints
 }
+?>
+      </div>
+
+      <div role="tabpanel" id="netsettings" class="tab-pane">
+	<p><?php echo _("Individual networks may be specified to override the default rule for an interface. For example, if interface eth0 is assigned to the 'External' zone, you can add a specific <strong>source</strong> network to the 'Trusted' zone. This will override the default rule for that interface, so traffic from that network will be treated as 'Trusted',  whilst all traffic NOT originating from that network is Filtered by the default rules."); ?></p>
+        <p><?php echo _("Note that several common settings are available on the 'Advanced' page."); ?></p>
+<?php
+$nets = $fw->getZoneNetworks();
+// Add a blank one to the bottom..
+$nets[" "] = "trusted";
+
+$z = $fw->getZones();
+
+// Now, loop through our networks and display them.
+$counter = 1;
+foreach ($nets as $net => $currentzone) {
+	if (trim($net)) {
+		$ro = "readonly disabled";
+	} else {
+		$ro = "";
+	}
+?>
+        <div class='element-container' id='element-<?php echo $counter; ?>'>
+          <div class='row'>
+            <div class='col-sm-4 col-md-3'>
+              <input type='text' name='netname[<?php echo $counter; ?>]' value='<?php echo $net; ?>' <?php echo $ro; ?>>
+            </div>
+            <div class='col-sm-8 col-md-9'>
+              <span class='radioset'>
+<?php
+	// Display the buttons
+	foreach ($z as $zn => $zone) {
+		if ($zn == "reject") {
+			// Don't show 'reject', as it's not intuitive. It adds a network to an
+			// interface that's defined as reject, so that'll be PERMITTED, and..
+			// yeah. Let's just avoid that.
+			continue;
+		}
+		if ($zn === $currentzone) {
+			$active = "active";
+			$checked = "checked";
+		} else {
+			$active = "";
+			$checked = "";
+		}
+		print "<input type='radio' name='net-$counter' id='net-$counter-$zn' value='$zn' $checked><label class='$active' for='net-$counter-$zn'>".$zone['name']."</label>\n";
+	}
+?>	
+              </span>
+<?php
+	// Add the 'remove' X if the net isn't empty
+	if (trim($net)) {
+		print "<button type='button' class='btn x-btn x-btn-danger fwbutton' data-counter='$counter' data-action='remove'><span class='glyphicon glyphicon-remove'></span></button>";
+		print "<button type='button' class='btn x-btn x-btn-warning fwbutton' data-counter='$counter' data-action='update'><span class='glyphicon glyphicon-pencil'></span></button>";
+	} else {
+		// Or a '+' add if it is.
+		print "<button type='button' class='btn btn-success x-btn x-btn-success fwbutton' data-counter='$counter' data-action='create'><span class='glyphicon glyphicon-plus'></span></button>";
+	}
+?>
+            </div>
+          </div>
+        </div>
+<?php
+	$counter++;
+} // foreach $nets
 ?>
       </div>
     </div>
