5e349308db7c0a8030b9f5c753987f10a664f56c
Rob Thomas <xrobau@gmail.com>
2015-08-17 13:44:56 +1000

    OK, now we have the services

--- phar/src/firewall.php
+++ phar/src/firewall.php
@@ -2,14 +2,46 @@
 
 require 'common.php';
 
-echo "I'm in firewall.php\n";
+if (posix_geteuid() !== 0) {
+	throw new \Exception("I must be run as root.");
+}
 
+// Update every 60 seconds.
+$period = 60;
+
+$lastfin = time() - $period;
 while(true) {
-	sleep(5);
-	print "A loop!\n";
+	// Check to see if we should restart
 	if (pharChanged()) {
 		print "It changed!\n";
 		exit;
 	}
+
+	if ($lastfin + $period < time()) {
+		// We finished more than $period ago. We can run again.
+		updateFirewallRules();
+		$lastfin = time();
+		continue;
+	} else {
+		// Sleep until we're ready to go again.
+		sleep($period/10);
+	}
+}
+
+function updateFirewallRules() {
+	// Signature validation
+	global $v;
+
+	// Asterisk user
+	$astuser = "asterisk";
+
+	// We want to switch to the asterisk user and ask for the port mappings.
+	if (!$v->checkFile("bin/getservices")) {
+		print "Can't validate bin/getservices";
+		// exit/throw...
+	}
+
+	exec("/usr/bin/su -c /var/www/html/admin/modules/firewall/bin/getservices $astuser", $out, $ret);
+	print_r($out);
 }
 
