69b103cdf4f19b6f4bdeb42c9342d80a7d986465
Rob Thomas <xrobau@gmail.com>
2015-09-04 16:56:06 +1000

    Random checkin.

--- drivers/Iptables.class.php
+++ drivers/Iptables.class.php
@@ -5,11 +5,28 @@ namespace FreePBX\modules\Firewall\Drivers;
 // Iptables - Generic.
 class Iptables {
 
+	private $currentconf = false;
+
 	public function getZonesDetails() {
 		// Returns array( "zonename" => array("interfaces" => .., "services" => .., "sources" => .. ), 
 		//   "zonename" => .. 
 		//   "zonename => ..
 		// );
+		$default = array("interfaces" => array(), "services" => array(), "sources" => array());
+		$zones = array("reject" => $default, "external" => $default, "other" => $default,
+			"internal" => $default, "trusted" => $default);
+
+		$current = $this->getCurrentIptables();
+
+		// Check IPv4 for the interface and config settings. IPv6 should be identical. But,
+		// if it's broken for some reason, it may not be providing useful information.
+
+		if (!$this->isConfigured($current['ipv4'])) {
+			print "Not configured\n";
+			return $zones;
+		}
+
+		print_r($current);
 	}
 
 	public function getKnownNetworks() {
@@ -38,45 +55,67 @@ class Iptables {
 
 	// Root process
 	public function addNetworkToZone($zone = false, $network = false, $cidr = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Root process
 	public function removeNetworkFromZone($zone = false, $network = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Root process
 	public function changeNetworksZone($newzone = false, $network = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Root process
 	public function updateService($service = false, $ports = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Root process
 	public function updateServiceZones($service = false, $zones = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Root process
 	public function changeInterfaceZone($iface = false, $newzone = false) {
+		$this->checkFpbxFirewall();
 	}
 
 	// Driver Specific iptables stuff
 
 	// Root process
 	private function getCurrentIptables() {
-		// Parse iptables-save output
-		exec('/sbin/iptables-save 2>&1', $ipv4, $ret);
-		exec('/sbin/ip6tables-save 2>&1', $ipv6, $ret);
-		$retarr = array("ipv4" => $this->parseIptablesOutput($ipv4),
-			"ipv6" => $this->parseIptablesOutput($ipv6),
-		);
+		if (!$this->currentconf) {
+			// Parse iptables-save output
+			exec('/sbin/iptables-save 2>&1', $ipv4, $ret);
+			exec('/sbin/ip6tables-save 2>&1', $ipv6, $ret);
+			$this->currentconf = array(
+				"ipv4" => $this->parseIptablesOutput($ipv4),
+				"ipv6" => $this->parseIptablesOutput($ipv6),
+			);
+		}
+		return $this->currentconf;
+	}
 
-		return $retarr;
+	private function checkFpbxFirewall() {
+		throw new \Exception("OK, start here");
+
+		if (!$this->isConfigured()) {
+			// Make sure we've cleaned up
+			$this->cleanOurRules();
+			// And add our defaults in
+		}
+
 	}
 
 	private function parseIptablesOutput($iptsave) {
 		$table = "unknown";
-		foreach ($output as $line) {
+
+		$conf = array();
+
+		foreach ($iptsave as $line) {
 			if (empty($line)) {
 				continue;
 			}
@@ -92,7 +131,7 @@ class Iptables {
 				// It's a chain definition
 				list($chain, $stuff) = explode(" ", $line);
 				$chain = substr($chain, 1);
-				$this->currentconf[$table][$chain] = array();
+				$conf[$table][$chain] = array();
 				continue;
 			}
 
@@ -103,13 +142,75 @@ class Iptables {
 			$linearr = explode(" ", $line);
 			array_shift($linearr);
 			$chain = array_shift($linearr);
-			$this->currentconf[$table][$chain][] = join(" ", $linearr);
+			$conf[$table][$chain][] = join(" ", $linearr);
 		}
 
 		// Make sure we have SOMETHING there.
-		if (!isset($this->currentconf['filter'])) {
-			$this->currentconf['filter'] = array("INPUT" => array());
+		if (!isset($conf['filter'])) {
+			$conf['filter'] = array("INPUT" => array());
 		}
+
+		return $conf;
+	}
+
+	private function isConfigured($ipt) {
+		// Check to see that our firewall rule is the first one.
+		if (!isset($ipt['filter']) || !isset($ipt['filter']['INPUT'][0])) {
+			return false;
+		}
+
+		// OK, so what IS the first rule in input?
+		if ($ipt['filter']['INPUT'][0] === "-j fpbxfirewall") {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private function parseFilter($arr) {
+		if (!is_array($arr)) {
+			throw new \Exception("Wasn't given an array");
+		}
+
+		$str = "";
+		if (isset($arr['int'])) { $str .= "-i ".$arr['int']." "; }
+		if (isset($arr['proto'])) {
+			$str .= "-p ".$arr['proto']." ";
+			if (isset($arr['dport'])) {
+				if (strpos($arr['dport'], ',') === false) {
+					$str .= "-m ".$arr['proto']." ";
+				} else {
+					$str .= "-m multiport ";
+				}
+			}
+		}
+		if (isset($arr['src'])) {
+			// TODO: Check with ipv6
+			list($src) = explode(":", $arr['src']); // eg, $src = explode(":", $arr['src'])[0];
+			if (strpos($src, "/") === false) {
+				$src .= "/32";
+			}
+			$str .= "-s $src ";
+		}
+		if (isset($arr['dport'])) {
+			$str .= "--dport ".$arr['dport']." ";
+		}
+		if (isset($arr['out'])) {
+			$str .= "-o ".$arr['out']." ";
+		}
+		if (isset($arr['other'])) {
+			$str .= $arr['other']." ";
+		}
+		if (isset($arr['jump'])) {
+			$str .= "-j ".$arr['jump'];
+		}
+
+		if (!$str) {
+			throw new \Exception("Wat. Nothing? ".json_encode($arr));
+		}
+
+		// Make sure nothing can escape from this.
+		return escapeshellcmd($str);
 	}
 }
 
