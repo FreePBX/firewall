c01afdf3d568d7d6c812e2aa6296b45e5c7cef94
Rob Thomas <xrobau@gmail.com>
2015-09-16 01:32:01 +0000

    OK, fixed modprobe options

--- phar/src/modprobe.php
+++ phar/src/modprobe.php
@@ -12,16 +12,20 @@ class Modprobe {
 	}
 
 	public function needsUpdate() {
-		if (!file_exists("/etc/modprobe.d/ipt_recent")) {
+		if (!file_exists("/etc/modprobe.d/ipt_recent.conf")) {
+			return true;
+		}
+		if (!file_exists("/etc/modprobe.d/xt_recent.conf")) {
 			return true;
 		}
 		return false;
 	}
 
 	public function updateModprobe() {
-		$conf = "options ipt_recent ip_pkt_list_tot=2048\n";
-
-		file_put_contents("/etc/modprobe.d/ipt_recent", $conf);
+		$conf = "options ipt_recent options ip_list_tot=4096 ip_pkt_list_tot=254\n";
+		file_put_contents("/etc/modprobe.d/ipt_recent.conf", $conf);
+		$conf = "options xt_recent options ip_list_tot=4096 ip_pkt_list_tot=254\n";
+		file_put_contents("/etc/modprobe.d/xt_recent.conf", $conf);
 		`depmod -a`;
 		return true;
 	}
