72bd8753e666d42d697c8f12153e4e719a23c911
Rob Thomas <xrobau@gmail.com>
2015-09-17 02:12:22 +0000

    Remove advanced page, it wasn't needed

--- views/view.smart.php
+++ views/view.smart.php
@@ -2,80 +2,61 @@
 $ssf = "Sangoma "._("Smart Firewall");
 
 $period = _("Refresh Period");
+$periods = array("normal" => _("Normal"), "fast" => _("Fast"), "slow" => _("Slow"));
 
-$tr = _("Temporary Registrations");
-$to = _("Registration Timeout");
 
 ?>
 <h3><?php echo $ssf; ?></h3>
 <?php
 $docs = array(
 	"$ssf "._("is a fully integrated and tightly coupled firewall that constantly monitors the remote clients allowed to connect to this machine, and automatically allows access from expected hosts."),
-	_("This is done by a small process that runs on your FreePBX server that automatically updates firewall rules based on the current trunk and extension configuration of FreePBX. When Smart Firewall is turned on, there is no need to explicitly add exclusions for SIP or IAX peers, and a best effort is made to automatically allow remote extensions through the firewall."),
+	_("This is done by a small process that runs on your FreePBX server that automatically updates firewall rules based on the current trunk and extension configuration of FreePBX."),
+	_("There is no need to explicitly add exclusions for SIP or IAX peers, as they are automatically allowed through the firewall after successfully registering."),
+	_("After an endpoint is registered, an automatic firewall rule is additionally granted to allow that IP Address to access UCP, if UCP is enabled."),
+	_("The 'Refresh Period' is how often the firewall updates its rules. You would set it to 'fast' if you have many endpoints that are constantly registering and de-registering, or 'slow' if you are on a low powered machine."),
 );
 
 foreach ($docs as $p) {
 	print "<p>$p</p>\n";
 }
-print "<div class='row'>\n";
+?>
 
-// Are we actually enabled?
-if (isset($smart['ssf']) && $smart['ssf']) {
-	$disabled = false;
-} else {
-	$disabled = true;
+<div class='row'>
+  <div class='form-horizontal clearfix'>
+    <div class='col-sm-4'>
+      <label class='control-label' for='period'><?php echo $period; ?></label>
+    </div>
+    <div class='col-sm-8'>
+      <select class='form-control' id='period'>
+<?php
+$current = \FreePBX::Firewall()->getConfig('refreshperiod');
+if (!$current) {
+	$current = "normal";
+	\FreePBX::Firewall()->setConfig('refreshperiod', 'normal');
 }
 
-showRadio("ssf", $ssf, !$disabled, false);
-showText("period", $period, $smart['period'], $disabled);
-showRadio("tempreg", $tr, $smart['tempreg'], $disabled);
-showText("tempregto", $to, $smart['tempregto'], $disabled);
-
-print "</div>\n";
-
-// Disabled is the prop disabled, not a value.
-function showRadio($k, $title, $val = false, $disabled = false) {
-	$ena = $dis = "";
-	$e = _("Enabled");
-	$d = _("Disabled");
-	if ($val) {
-		$ena = "checked";
+foreach ($periods as $name => $val) {
+	if ($current == $name) {
+		$selected = "selected";
 	} else {
-		$dis = "checked";
+		$selected = "";
 	}
-
-	if ($disabled) {
-		$pd = " disabled";
-	} else {
-		$pd = "";
-	}
-	print "<div class='form-horizontal clearfix'>\n";
-	print " <div class='col-sm-4'>\n";
-	print "  <label class='control-label' for='$k'>$title</label>\n";
-	print " </div>\n";
-	print " <div class='col-sm-8'>\n";
-	print "  <span class='radioset'>\n";
-	print "   <input type='radio' name='$k' id='${k}enabled' $ena $pd><label for='${k}enabled'>$e</label>\n";
-	print "   <input type='radio' name='$k' id='${k}disabled' $dis $pd><label for='${k}disabled'>$d</label>\n";
-	print "  </span>\n";
-	print " </div>\n";
-	print "</div>\n";
+	print "<option value='$name' $selected>$val</option>\n";
 }
+?>
+      </select>
+    </div>
+  </div>
+</div>
 
-// Disabled is the prop disabled, not a value.
-function showText($k, $title, $val = "0", $disabled = false) {
-	if ($disabled) {
-		$pd = " disabled";
-	} else {
-		$pd = "";
-	}
-	print "<div class='form-horizontal clearfix'>\n";
-	print " <div class='col-sm-4'>\n";
-	print "  <label class='control-label' for='$k'>$title</label>\n";
-	print " </div>\n";
-	print " <div class='col-sm-8'>\n";
-	print "  <input class='form-control' type='text' name='$k' id='$k' value='$val' $pd>\n";
-	print " </div>\n";
-	print "</div>\n";
-}
+<div class='row'>
+<div class='form-horizontal clearfix'>
+  <div class='col-sm-4'>
+    <label class='control-label' for='ssf'><?php echo $ssf; ?></label>
+  </div>
+  <div class='col-sm-8'>
+    <button type='button' class='btn btn-default'><?php echo _("Disable"); ?></button>
+  </div>
+</div>
+</div>
 
