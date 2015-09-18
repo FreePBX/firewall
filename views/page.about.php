975a2d05a106c65afb7c428a2080c5cfc27cced9
Rob Thomas <xrobau@gmail.com>
2015-09-18 02:03:31 +0000

    Clean up wording, as per dicko's suggestions

--- views/page.about.php
+++ views/page.about.php
@@ -20,13 +20,15 @@
 // Text for Shortcuts is here
 $rfc = array(
 	_("RFC1918/RFC4193"),
-	_("RFC1918 and RFC4193 are the RFCs that define the reserved, internal, network address space to be used when you're not directly connected to the internet, or do not want your address space routable. This adds the IPv4 networks 192.168.0.0/16, 172.16.0.0/12, and 10.0.0.0/8 and IPv6 networks fc00::/8 and fd00::/8 to the 'Trusted' zone, and excludes it from all firewalls. Be warned that if you are in a hosted environment (for example, AWS), and you enable this, you may be inadvertently allowing other hosted clients unrestricted access to your machine."),
+	_("RFC1918 and RFC4193 are the RFCs that define the reserved, internal, network address space to be used when you're not directly connected to the internet, or do not want your address space routable. This adds the IPv4 networks 192.168.0.0/16, 172.16.0.0/12, and 10.0.0.0/8 and IPv6 networks fc00::/8 and fd00::/8 to the 'Trusted' zone, and excludes it from all firewalls."),
+       _("<strong>Important Warning!</strong> If you are in a hosted environment (for example, AWS) and you enable this, you may be inadvertently allowing other hosted clients unrestricted access to your machine! Please use common sense to make sure that you are only allowing known trusted networks."),
 	_("Add to Trusted"),
 );
 
 $ym = array(
 	_("Your Client"),
-	_("This explicitly grants permission to the machine that is managing the firewall service now. If you select 'Add Network', it will add the entire network that the server sees you coming from (%s), or if you select 'Add Host' it will only add the individual IP address (%s). When starting to configure your firewall, it is wise to enable this to ensure you don't lock yourself out of your machine. If you are coming from an IPv6 Network, it is unwise to add just your Host, as MAC address changes of your client will change your IP address."),
+	_("This explicitly grants permission to the machine that is managing the firewall service now. If you select 'Add Network', it will add the entire network that the server sees you coming from (%s), or if you select 'Add Host' it will only add the individual IP address (%s). When starting to configure your firewall, it is wise to enable this to ensure you don't lock yourself out of your machine."),
+	_("If you are coming from an IPv6 Network, it <strong>not recommended</strong> to only add your 'Host', as MAC address changes, or IPv6 Security Extensions, will randomly and unexpectedly change your IP address. Ensure you add the complete network."),
 	_("Add Network"),
 	_("Add Host"),
 );
@@ -37,24 +39,26 @@ $thishost = $fw->detectHost();
 ?>
       <div role="tabpanel" id="shortcuts" class="tab-pane">
         <div class='container-fluid'>
-	  <p><?php echo _("This allows you to simply add a pre-configured set of networks to your trusted zone. Once you have added your selections, you can fine-tune them, if required, on the Networks tab."); ?></p>
+	  <p><?php echo _("This allows you to simply add a pre-configured set of networks to your trusted zone. Once you have added your selections, you can fine-tune them, if required, <a href='?display=firewall&page=zones&tab=netsettings'>on the Networks tab.</a>"); ?></p>
           <div class='panel panel-default'>
 	    <div class='panel-body'>
 	      <h3><?php echo $rfc[0]; ?></h3>
 	      <p><?php echo $rfc[1]; ?></p>
+	      <p><?php echo $rfc[2]; ?></p>
             </div>
             <div class='panel-footer clearfix'>
-	      <button type='button' class='btn btn-default pull-right' id='addrfc'><?php echo $rfc[2]; ?></button>
+	      <button type='button' class='btn btn-default pull-right' id='addrfc'><?php echo $rfc[3]; ?></button>
             </div>
           </div>
           <div class='panel panel-default'>
             <div class='panel-body'>
 	      <h3><?php echo $ym[0]; ?></h3>
 	      <p><?php printf($ym[1], $thisnet, $thishost); ?></p>
+	      <p><?php echo $ym[2]; ?></p>
             </div>
             <div class='panel-footer clearfix'>
-	      <button type='button' class='btn btn-default pull-right' id='addnetwork'><?php echo $ym[2]; ?></button>
-	      <button type='button' class='btn btn-default pull-right' id='addhost'><?php echo $ym[3]; ?></button>
+	      <button type='button' class='btn btn-default pull-right' id='addnetwork'><?php echo $ym[3]; ?></button>
+	      <button type='button' class='btn btn-default pull-right' id='addhost'><?php echo $ym[4]; ?></button>
             </div>
           </div>
         </div>
