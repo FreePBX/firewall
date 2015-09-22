a7c94691217be78240628bf49b50b88512774f8a
Rob Thomas <xrobau@gmail.com>
2015-09-21 20:16:11 -0700

    Fix modprobe options

--- phar/src/modprobe.php
+++ phar/src/modprobe.php
@@ -22,9 +22,9 @@ class Modprobe {
 	}
 
 	public function updateModprobe() {
-		$conf = "options ipt_recent options ip_list_tot=4096 ip_pkt_list_tot=254\n";
+		$conf = "options ipt_recent ip_list_tot=4096 ip_pkt_list_tot=254\n";
 		file_put_contents("/etc/modprobe.d/ipt_recent.conf", $conf);
-		$conf = "options xt_recent options ip_list_tot=4096 ip_pkt_list_tot=254\n";
+		$conf = "options xt_recent ip_list_tot=4096 ip_pkt_list_tot=254\n";
 		file_put_contents("/etc/modprobe.d/xt_recent.conf", $conf);
 		`depmod -a`;
 		return true;
