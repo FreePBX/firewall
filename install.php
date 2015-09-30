5b46ae88e8538fed35aadcea6c25841047d49de5
Rob Thomas <xrobau@gmail.com>
2015-09-30 03:47:54 +0000

    Don't need that warning now, two upgrades is plenty

--- install.php
+++ install.php
@@ -6,10 +6,3 @@
 $file = "/var/spool/asterisk/incron/firewall.firewall";
 fclose(fopen($file, "c"));
 
-// Warn about firewall page changing location
-\FreePBX::create()->Notifications()->add_warning('firewall', 'hasmoved', _("Firewall Configuration has moved!"),
-	_("The Firewall configuration menu option has moved to 'Connectivity'"),
-	false,
-	true,
-	true);
-
