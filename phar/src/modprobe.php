414730ee28ad26dad502bca2f1ccc635c0149001
Rob Thomas <xrobau@gmail.com>
2015-10-18 06:39:18 +1000

    Ensure old mod.conf files are replaced

--- phar/src/modprobe.php
+++ phar/src/modprobe.php
@@ -18,6 +18,16 @@ class Modprobe {
 		if (!file_exists("/etc/modprobe.d/xt_recent.conf")) {
 			return true;
 		}
+
+		// This can be removed after all machines with the broken .conf files would have been upgraded.
+		// Say by 2016-01-01
+		if (hash_file("sha256", "/etc/modprobe.d/ipt_recent.conf") !== "5c47da497377a2def8b0944a0767736e2aefd6fe5f833eb38121646c41086461") {
+			return true;
+		}
+		if (hash_file("sha256", "/etc/modprobe.d/xt_recent.conf") !== "bf3cabcda96799f1b37cb26f7992b4c98f3a66fe8242ea5970f63de3d004c751") {
+			return true;
+		}
+
 		return false;
 	}
 
