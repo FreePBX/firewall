7daa92418f6130689e466448d1cb7ec53bc8773a
Philip Joseph <jphilip@sangoma.com>
2017-07-06 15:49:23 +0530

    FREEPBX-14207 Client Machine Not Detected as Being Trusted When It Is Part of a Trusted Subnet
    	Subnet calculation was not there, it was just compiring the ips
    added as the trusted .

--- Firewall.class.php
+++ Firewall.class.php
@@ -1013,11 +1013,75 @@ class Firewall extends \FreePBX_Helpers implements \BMO {
 			if ($zone !== "trusted") {
 				continue;
 			}
-			if ($n === $thisnet || $n === $thishost) {
+			// need to do the subnet calculation
+			$dq_host = strtok("$n", "/");
+			$cdr_nmask = strtok("/");
+
+			$bin_nmask = $this->cdrtobin($cdr_nmask);
+			$bin_wmask = $this->binnmtowm($bin_nmask);
+
+			$bin_host = $this->dqtobin($dq_host);
+			$bin_bcast = (str_pad(substr($bin_host,0,$cdr_nmask),32,1));
+			$bin_net = (str_pad(substr($bin_host,0,$cdr_nmask),32,0));
+			$bin_first = (str_pad(substr($bin_net,0,31),32,1));
+			$bin_last = (str_pad(substr($bin_bcast,0,31),32,0));
+
+			$iprange_start = $this->bintodq($bin_first);
+			$iprange_end = $this->bintodq($bin_last);
+			// let us take the thishost to compire with range
+			$client_host_ip = strtok("$thishost", "/");
+			if ($n === $thisnet || (ip2long($iprange_start) <= ip2long($client_host_ip) && ip2long($iprange_end) >= ip2long($client_host_ip) )) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	function dqtobin($dqin) {
+        $dq = explode(".",$dqin);
+        for ($i=0; $i<4 ; $i++) {
+			$bin[$i]=str_pad(decbin($dq[$i]), 8, "0", STR_PAD_LEFT);
+		}
+	return implode("",$bin);
+	}
+
+	function binnmtowm($binin){
+		$binin=rtrim($binin, "0");
+		if (!preg_match("/0/",$binin,$matches) ){
+			return str_pad(str_replace("1","0",$binin), 32, "1");
+		} else return "1010101010101010101010101010101010101010";
+	}
+
+	function bintocdr ($binin){
+		return strlen(rtrim($binin,"0"));
+	}
+
+	function bintodq ($binin) {
+		if ($binin=="N/A") return $binin;
+			$binin=explode(".", chunk_split($binin,8,"."));
+			for ($i=0; $i<4 ; $i++) {
+				$dq[$i]=bindec($binin[$i]);
+			}
+	    return implode(".",$dq) ;
+	}
+
+	function bintoint ($binin){
+		return bindec($binin);
+	}
+
+	function binwmtonm($binin){
+		$binin=rtrim($binin, "1");
+		if (!preg_match("/1/",$binin,$matches)){
+			return str_pad(str_replace("0","1",$binin), 32, "0");
+		} else return "1010101010101010101010101010101010101010";
+	}
+
+	function cdrtobin ($cdrin){
+		return str_pad(str_pad("", $cdrin, "1"), 32, "0");
+	}
+
+	function inttobin ($intin) {
+		return str_pad(decbin($intin), 32, "0", STR_PAD_LEFT);
 	}
 
 	public function rfcNetsAdded() {
