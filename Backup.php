01d43f081f1106fe6fe877749fa6a1b2212da57d
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 17:19:27 -0500

    [Module Tag script: firewall 15.0.3] fixing legacy restore

--- Backup.php
+++ Backup.php
@@ -2,13 +2,8 @@
 namespace FreePBX\modules\Firewall;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-    public function runBackup($id,$transaction){
-        $kvstoreids = $this->FreePBX->Firewall->getAllids();
-        $kvstoreids[] = 'noid';
-        $settings = [];
-        foreach ($kvstoreids as $value) {
-            $settings[$value] = $this->FreePBX->Firewall->getAll($value);
-        }
-        $this->addConfigs($settings);
-    }
+	public function runBackup($id,$transaction){
+		$settings = $this->dumpKVStore();
+		$this->addConfigs($settings);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,10 +2,12 @@
 namespace FreePBX\modules\Firewall;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-    public function runRestore($jobid){
-        $settings = $this->getConfigs();
-        foreach ($settings as $key => $value) {
-            $this->FreePBX->Firewall->setMultiConfig($value, $key);
-        }
-    }
+	public function runRestore($jobid){
+		$settings = $this->getConfigs();
+		$this->importKVStore($settings);
+	}
+
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyKvstore($pdo);
+	}
 }
