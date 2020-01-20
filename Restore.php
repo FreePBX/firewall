bbbb39162ae780539f1a03bdbc71650a7a5a9ff3
Jaiver Pastor <jpastor@cerebelum.net>
2020-01-20 18:59:27 +0100

    Fix issues FREEPBX-21053

--- Restore.php
+++ Restore.php
@@ -5,6 +5,30 @@ class Restore Extends Base\RestoreBase{
 	public function runRestore(){
 		$settings = $this->getConfigs();
 		$this->importKVStore($settings);
+
+		$files = $this->getFiles();
+		$nfiles = 0;
+		foreach($files as $file){
+			if($file->getType() != 'firewall rules') { 
+				continue;
+			}
+
+			$dest = $file->getPathTo().'/'.$file->getFilename();
+			$source = $this->tmpdir.'/files'.$dest;
+
+			if(file_exists($source)){
+				if(file_exists($dest)) {
+					$dest_old = $dest."_".date('YmdHisu');
+					rename($dest, $dest_old);
+					$this->log(sprintf(_("The destine file exist, save backup file: %s"), $dest_old),'INFO');
+				}
+				copy($source, $dest);
+				chown($dest, "root");
+				chmod($dest, 0644);
+				$nfiles++;
+			}
+		}
+		$this->log(sprintf(_("%s Files Restored"), $nfiles++),'INFO');
 	}
 
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
