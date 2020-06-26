fb8ce5efb9a21b843541fb6b9a9249e320fa6cca
Jaiver Pastor <jpastor@cerebelum.net>
2020-06-26 12:22:34 +0200

    Show btn Save and Apply only when custom rules are activated.

--- assets/js/views/advanced.js
+++ assets/js/views/advanced.js
@@ -253,6 +253,7 @@ class control_module {
 
 var mod_firewall_control = new control_module("firewall", false);
 mod_firewall_control.onTabChange = function (e, tab_old) {
+	//
 };
 mod_firewall_control.add_tab_control("advanced_customrules", advanced_custom_rules_setfocus, advanced_custom_rules_unsetfocus);
 
@@ -375,9 +376,11 @@ function advanced_custom_rules_status(c, e) {
 			if (data.status == true) {
 				if (data.message == "enabled") {
 					$(".msg-warning-custom-rules-disabled:visible", e).hide("slow");
+					$(".btn-save-apply-custom-rules:hidden", e).show("slow");
 				}
 				else {
 					$(".msg-warning-custom-rules-disabled:hidden", e).show("slow");
+					$(".btn-save-apply-custom-rules:visible", e).hide("slow");
 				}
 			}
 		})
