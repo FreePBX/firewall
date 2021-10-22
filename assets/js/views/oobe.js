e92946871b012288dae8f30aa0eb382b08f95484
Franck Danard <fdanard@sangoma.com>
2021-10-22 14:37:28 +0200

    FREEPBX-22943 Firewall leaving interface as Trusted

--- assets/js/views/oobe.js
+++ assets/js/views/oobe.js
@@ -55,9 +55,9 @@ function processQuestion(q) {
 	h += "</div>";
 	// Buttons.
 	if (q.default === "yes") {
-		b = "<div class='pull-right clearfix'><button type='button' class='btn btn-default' onClick='buttonNo()'>No</button><button type='button' class='btn btn-default' onclick='buttonYes()'>Yes</button></div>";
+		b = "<div class='pull-right clearfix'><button type='button' class='btn btn-default' onClick='buttonNo()'>No</button><button type='button' class='btn btn-default active' onclick='buttonYes()'>Yes</button></div>";
 	} else {
-		b = "<div class='pull-right clearfix'><button type='button' onClick='buttonNo()' class='btn btn-default'>No</button><button type='button' onClick='buttonYes()' class='btn btn-default'>Yes</button></div>";
+		b = "<div class='pull-right clearfix'><button type='button' class='btn btn-default active' onClick='buttonNo()'>No</button><button type='button' class='btn btn-default' onClick='buttonYes()'>Yes</button></div>";
 	}
 	$("#qdiv").html(h);
 	$("#buttonsdiv").html(b);
@@ -67,7 +67,6 @@ function processQuestion(q) {
 		$("#alertsdiv").html("<div class='alert alert-"+q.alerttype+"'>"+q.alert+"</div>").show();
 	}
 }
-
 
 function buttonYes() {
 	console.log("Yes clicked");
@@ -86,4 +85,3 @@ function submitAnswer(q, ans) {
 		success: function(data) { getQuestion(); },
 	});
 }
-
