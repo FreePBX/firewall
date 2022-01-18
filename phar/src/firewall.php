4912cbcd2d07180b6365b48dba55733d2c5b23af
Franck Danard <fdanard@sangoma.com>
2022-01-18 10:59:30 +0100

    FREEPBX-23241 Issue with getServices()

--- phar/src/firewall.php
+++ phar/src/firewall.php
@@ -855,7 +855,7 @@ function getServices() {
 		chmod("/var/www/html/admin/modules/firewall/bin/getservices", 0755);
 	}
 
-	exec("/sbin/runuser $astuser -c /var/www/html/admin/modules/firewall/bin/getservices", $out, $ret);
+	exec("/usr/sbin/runuser $astuser -c /var/www/html/admin/modules/firewall/bin/getservices", $out, $ret);
 	$getservices = @json_decode($out[0], true);
 	if (!is_array($getservices) || !isset($getservices['smartports'])) {
 		fwLog("Unparseable output from getservices - ".json_encode($out)." - returned $ret");
